66cd66653a164ee84517dcd38d37f53ab85a8a2c
Brian <153983527+Briel8@users.noreply.github.com>
2024-02-16 19:53:57 +0200

    Added the point recording feature and the function that plays the game five times.

--- script.js
+++ script.js
@@ -4,23 +4,37 @@ function getComputerChoice(){
     return options[Math.floor(Math.random() * 3)];
 }
 
+// For storing the players points.
+let playerPoints = 0;
+let computerPoints = 0;
+
 function playRound(playerSelection, computerSelection){
     const playerChoice = playerSelection.toLowerCase();       // to make sure input data is the same.
     const computerChoice = computerSelection.toLowerCase();
     // Compare the choices.
     if (playerChoice === computerChoice){
+        playerPoints += 1;
+        computerPoints += 1;
         return `It's a tie! The computer has picked ${computerChoice} too.`;
     }else if(playerChoice === "rock" && computerChoice === "paper"){
+        computerPoints += 2; // the computer gains points.
         return `You loose! ${computerChoice} beats ${playerChoice}`;
     }else if(playerChoice === "paper" && computerChoice === "scissors"){
+        computerPoints += 2; // the computer gains points.
         return `You loose! ${computerChoice} beats ${playerChoice}`;
     }else if(playerChoice === "scissors" && computerChoice === "rock"){
+        computerPoints += 2; // the computer gains points.
         return `You loose! ${computerChoice} beats ${playerChoice}`;
     }else{
+        playerPoints += 2;
         return `You win! ${playerChoice} beats ${computerChoice}`;
     }
 }
 
 const computerSelection = getComputerChoice();
 const playerSelection = "paper";
-console.log(playRound(playerSelection, computerSelection));
+function playGame(){
+    for (let i = 0; i >= 5; i++){
+        playRound(playerSelection, computerSelection);
+    }
+};
