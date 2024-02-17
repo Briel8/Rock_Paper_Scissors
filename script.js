f01879ffc5e33e2e19ce5ca38f3c477550422bb5
Brian <153983527+Briel8@users.noreply.github.com>
2024-02-17 21:03:28 +0200

    Initial working stage.
    At this point the program is working according.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function getComputerChoice(){
+function getcomputerSelection(){
     // returns a random string from the array.
     const options = ["Rock", "Paper", "Scissors"];
-    return options[Math.floor(Math.random() * 3)];
+    return options[Math.floor(Math.random() * 3)].toLowerCase();
 }
 
 // For storing the players points.
@@ -9,32 +9,47 @@ let playerPoints = 0;
 let computerPoints = 0;
 
 function playRound(playerSelection, computerSelection){
-    const playerChoice = playerSelection.toLowerCase();       // to make sure input data is the same.
-    const computerChoice = computerSelection.toLowerCase();
     // Compare the choices.
-    if (playerChoice === computerChoice){
+    if (playerSelection === computerSelection){
         playerPoints += 1;
         computerPoints += 1;
-        return `It's a tie! The computer has picked ${computerChoice} too.`;
-    }else if(playerChoice === "rock" && computerChoice === "paper"){
+        return `It's a tie! Computer: ${computerSelection} - player: ${playerSelection}`;
+    }else if(playerSelection === "rock" && computerSelection === "paper"){
         computerPoints += 2; // the computer gains points.
-        return `You loose! ${computerChoice} beats ${playerChoice}`;
-    }else if(playerChoice === "paper" && computerChoice === "scissors"){
+        return `You loose! ${computerSelection} beats ${playerSelection} computer has ${computerPoints} points`;
+    }else if(playerSelection === "paper" && computerSelection === "scissors"){
         computerPoints += 2; // the computer gains points.
-        return `You loose! ${computerChoice} beats ${playerChoice}`;
-    }else if(playerChoice === "scissors" && computerChoice === "rock"){
+        return `You loose! ${computerSelection} beats ${playerSelection} computer has ${computerPoints} points`;
+    }else if(playerSelection === "scissors" && computerSelection === "rock"){
         computerPoints += 2; // the computer gains points.
-        return `You loose! ${computerChoice} beats ${playerChoice}`;
+        return `You loose! ${computerSelection} beats ${playerSelection} computer has ${computerPoints} points`;
     }else{
         playerPoints += 2;
-        return `You win! ${playerChoice} beats ${computerChoice}`;
+        return `You win! ${playerSelection} beats ${computerSelection} you have ${playerPoints} points`;
     }
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = "paper";
+
 function playGame(){
-    for (let i = 0; i >= 5; i++){
-        playRound(playerSelection, computerSelection);
+    
+    computerPoints = 0;
+    playerPoints = 0;
+
+    let i = 1;
+    while (i <= 5){
+        const computerSelection = getcomputerSelection();
+        const playerSelection = prompt("Choose! Rock Paper Scissors.").toLowerCase();
+        console.log(playRound(playerSelection, computerSelection));
+        i++;
     }
-};
+
+    // Now for the results.
+    
+    if (playerPoints === computerPoints){ // Spoils Shared.
+        console.log(`Overal, It's a tie. Computer ${computerPoints} - You ${playerPoints}`);
+    }else{
+        console.log(`Scores: Computer ${computerPoints} - You ${playerPoints}`);
+    }
+}
+
+playGame();
