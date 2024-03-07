f04eb4eac0e7dd722d4b15b1dda084e89b2b2340
Brian <153983527+Briel8@users.noreply.github.com>
2024-03-07 05:31:25 +0200

    Added a Graphical user interface
    Including three buttons of rock paper scissors respectively
    Added event listeners to the three buttons

--- script.js
+++ script.js
@@ -1,55 +1,60 @@
+let playerSelection = '';
+let computerSelection = '';
+let playerScore = 0;
+let computerScore = 0;
+
 function getcomputerSelection(){
     // returns a random string from the array.
     const options = ["Rock", "Paper", "Scissors"];
     return options[Math.floor(Math.random() * 3)].toLowerCase();
 }
 
-// For storing the players points.
-let playerPoints = 0;
-let computerPoints = 0;
 
 function playRound(playerSelection, computerSelection){
+    
     // Compare the choices.
     if (playerSelection === computerSelection){
-        playerPoints += 1;
-        computerPoints += 1;
+        playerScore += 1;
+        computerScore += 1;
         return `It's a tie! Computer: ${computerSelection} - player: ${playerSelection}`;
     }else if(playerSelection === "rock" && computerSelection === "paper"){
-        computerPoints += 2; // the computer gains points.
-        return `You loose! ${computerSelection} beats ${playerSelection} computer has ${computerPoints} points`;
+        computerScore += 1;
+        return `You loose! ${computerSelection} beats ${playerSelection}`;
     }else if(playerSelection === "paper" && computerSelection === "scissors"){
-        computerPoints += 2; // the computer gains points.
-        return `You loose! ${computerSelection} beats ${playerSelection} computer has ${computerPoints} points`;
+        computerScore += 1;
+        return `You loose! ${computerSelection} beats ${playerSelection}`;
     }else if(playerSelection === "scissors" && computerSelection === "rock"){
-        computerPoints += 2; // the computer gains points.
-        return `You loose! ${computerSelection} beats ${playerSelection} computer has ${computerPoints} points`;
+        computerScore += 1;
+        return `You loose! ${computerSelection} beats ${playerSelection}`;
     }else{
-        playerPoints += 2;
-        return `You win! ${playerSelection} beats ${computerSelection} you have ${playerPoints} points`;
+        playerScore += 1;
+        return `You win! ${playerSelection} beats ${computerSelection}`;
     }
 }
 
-
-function playGame(){
-    
-    computerPoints = 0;
-    playerPoints = 0;
-
-    let i = 1;
-    while (i <= 5){
-        const computerSelection = getcomputerSelection();
-        const playerSelection = prompt("Choose! Rock Paper Scissors.").toLowerCase();
-        console.log(playRound(playerSelection, computerSelection));
-        i++;
-    }
-
-    // Now for the results.
-    
-    if (playerPoints === computerPoints){ // Spoils Shared.
-        console.log(`Overal, It's a tie. Computer ${computerPoints} - You ${playerPoints}`);
-    }else{
-        console.log(`Scores: Computer ${computerPoints} - You ${playerPoints}`);
+function keepScores(rounds){
+    if (playerScore >= rounds || computerScore >= rounds){
+        resultDiv.textContent = `Player ${playerScore} : Computer ${computerScore}`;
+        playerScore = 0;
+        computerScore = 0;
     }
 }
 
-playGame();
+const body = document.querySelector('body');
+const resultDiv = document.querySelector('.resultDiv');
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        
+        playerSelection = button.id;
+        computerSelection = getcomputerSelection();
+        resultDiv.textContent = playRound(playerSelection, computerSelection);
+        //console.log(`Player ${playerScore} : ${computerScore} Computer`); // Debugging
+        keepScores(5);
+
+        body.appendChild(resultDiv);
+
+    });
+});
+
